78961af762d47a9681ba2febcf44baf9742e73f0
R1cardoX <39510364+R1cardoX@users.noreply.github.com>
2018-06-18 23:07:04 +0800

    Add files via upload

--- weibo_async.py
+++ weibo_async.py
@@ -1,6 +1,5 @@
 import requests
 import random
-from selenium import webdriver
 import time
 from bs4 import BeautifulSoup
 import urllib.error
@@ -12,29 +11,63 @@ import http.cookiejar
 import base64
 import urllib
 import json
+import pandas as pd
+import numpy as np
 import binascii
 import aiohttp
 import asyncio
 import multiprocessing as mp
-from selenium.webdriver.chrome.options import Options
-from selenium import webdriver
-from lxml import etree
-class Launcher:
-    def __init__(self,username,password):
-        self.username = username
-        self.password = password
-    def SetSession(self,session):
-        self.session = session
-    def get_encrypted_name(self):
-        username_urllike = urllib.request.quote(self.username)
+class Scrapy:
+    t0 = time.time()
+    count = 0
+    login_url = "https://weibo.com"
+    def __init__(self,time = 10):
+        self.time = time
+        self.user_pool = self.init_users()
+        self.headers_pool = self.init_headers()
+        self.cookies_pool = self.init_cookies()
+
+    def init_headers(self):
+        file = open('headers','r')
+        headers_group = []
+        for headers in file.readlines():
+            headers = {"User-Agent":headers.strip('\n')}
+            headers_group.append(headers)
+        return headers_group
+
+    def init_cookies(self):
+        cookies_pool = []
+        i = 0
+        for _ in range(10):
+            for user in self.user_pool:
+                cookies = self.login(user)
+                print('Get cookies No.',i+1,"with user:",user[0])
+                cookies_pool.append(cookies)
+                time.sleep(1)
+                i += 1
+        print('\n................................cookies_pool len:',len(cookies_pool))
+        self.t0 = time.time()
+        return cookies_pool
+    
+    def init_users(self):
+        file = open('userform','r')#格式 用户名:密码
+        users_group = []
+        for users in file.readlines():
+            users = (users.strip('\n').split(':')[0],users.strip('\n').split(':')[1])
+            users_group.append(users)
+        return users_group
+
+    def get_encrypted_name(self,user):
+        username_urllike = urllib.request.quote(user[0])
         username_encrypted = base64.b64encode(bytes(username_urllike,encoding = 'utf-8'))
         return username_encrypted.decode('utf-8')
-    def get_prelogin_args(self):
+
+    def get_prelogin_args(self,user):
         '''
         模拟预登陆 获取服务器返回的 nonce ，servertime，pub_key 等信息
         '''
         json_pattern = re.compile('\((.*)\)')
-        url = 'https://login.sina.com.cn/sso/prelogin.php?entry=weibo&callback=sinaSSOController.preloginCallBack&su=' + self.get_encrypted_name() + '&rsakt=mod&checkpin=1&client=ssologin.js(v1.4.19)'
+        url = 'https://login.sina.com.cn/sso/prelogin.php?entry=weibo&callback=sinaSSOController.preloginCallBack&su=' + self.get_encrypted_name(user) + '&rsakt=mod&checkpin=1&client=ssologin.js(v1.4.19)'
         try:
             request = urllib.request.Request(url)
             response = urllib.request.urlopen(request)
@@ -45,21 +78,17 @@ class Launcher:
         except urllib.error as e:
             print("%d"%e.code)
             return None
-    def get_encrypted_pw(self,data):
+
+    def get_encrypted_pw(self,data,user):
         rsa_e = 65537
-        pw_string = str(data['servertime']) + '\t' + str(data['nonce']) + '\n' +str(self.password)
+        pw_string = str(data['servertime']) + '\t' + str(data['nonce']) + '\n' +str(user[1])
         key = rsa.PublicKey(int(data['pubkey'],16),rsa_e)
         pw_encypted = rsa.encrypt(pw_string.encode('utf-8'),key)
         self.password = ''
         passwd = binascii.b2a_hex(pw_encypted)
         return passwd
-    def enableCookies(self):
-        cookie_container = http.cookiejar.CookieJar()
-        cookie_support = urllib.request.HTTPCookieProcessor(cookie_container)
-        opener = urllib.request.build_opener(cookie_support,urllib.request.HTTPHandler)
-        urllib.request.install_opener(opener)
         
-    def build_post_data(self,raw):
+    def build_post_data(self,raw,user):
         post_data = {
             "entry": "weibo",
             "gateway": "1",
@@ -68,13 +97,13 @@ class Launcher:
             "useticket": "1",
             "pagerefer": "https://login.sina.com.cn/crossdomain2.php?action=logout&r=https%3A%2F%2Fweibo.com%2Flogout.php%3Fbackurl%3D%252F",
             "vsnf": "1",
-            "su": self.get_encrypted_name(),
+            "su": self.get_encrypted_name(user),
             "service": "miniblog",
             "servertime": raw['servertime'],
             "nonce": raw['nonce'],
             "pwencode": "rsa2",
             "rsakv": raw['rsakv'],
-            "sp": self.get_encrypted_pw(raw),
+            "sp": self.get_encrypted_pw(raw,user),
             "sr": "1440*900",
             "encoding": "UTF-8",
             "prelt": "329",
@@ -83,168 +112,176 @@ class Launcher:
         }
         #data = urllib.parse.urlencode(post_data).encode('utf-8')
         return post_data
-    def login(self):
+
+    def login(self,user):
         url = 'https://login.sina.com.cn/sso/login.php?client=ssologin.js(v1.4.19)'
         p = re.compile('location\.replace\(\"(.*?)\"\)')
         p1 = re.compile('location\.replace\(\'(.*?)\'\)')
         p2 = re.compile(r'"uniqueid":"(\d*?)"')
-        self.enableCookies()
-        data = self.get_prelogin_args()
-        post_data = self.build_post_data(data)
-        headers = {
-            "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_4) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/66.0.3359.181 Safari/537.36"}
+        headers = random.sample(self.headers_pool,1)[0]
+        data = self.get_prelogin_args(user)
+        post_data = self.build_post_data(data,user)
         try:
-            request = self.session.post(url = url,data = post_data,headers = headers)
+            request = requests.post(url = url,data = post_data,headers = headers)
             login_url = p.search(request.text).group(1)
         except:
             print("1st login errer")
-            return None,None
+            print("..............................Error html:\n",request)
+            return None
         try:
-            login_html = self.session.get(login_url,headers = headers).text
+            login_html = requests.get(login_url,headers = headers).text
             login_url = p1.search(login_html).group(1)
         except:
             print("2nd jump errer")
-            return None,None
+            print("..............................Error html:\n",login_html)
+            return None
         try:
-            login_html = self.session.get(login_url,headers = headers).text
+            login_html = requests.get(login_url,headers = headers).text
             login_url = 'https://weibo.com/u/' + p2.search(login_html).group(1)
         except:
             print("3rd jump errer")
-            return None,None
-        return login_url,request.cookies
-    async def get_att_url(self,url,headers,cookies):
-        try:
-            async with aiohttp.ClientSession(cookies = cookies) as session:
-                async with session.get(url,headers = headers) as r:
-                    html =await r.text()
-                    await asyncio.sleep(0.5)
-                    return html
-        except:
+            print("..............................Error html:\n",login_html)
             return None
-    async def get_some_att(self,url,headers,cookies):
-        async with aiohttp.ClientSession(cookies = cookies) as session:
-            async with session.get(url,headers = headers) as r:
-                html =await r.text()
-                await asyncio.sleep(0.5)
-                return html
+        self.login_url = login_url
+        return request.cookies
 
-def analyse_some_att(html):
-    p = re.compile(r'action-data=\\"uid=(\d{1,15})&nick=(.+?)\\">')
-    soup = BeautifulSoup(html,'lxml')
-    _title = soup.find("title")
-    if _title:
-        title = _title.get_text()
-    else:
-        title = _title
-    att = p.findall(html)
-    url_list = []
-    for _id in att:
-        url = 'https://weibo.com/u/' + _id[0] + '?from=myfollow_all'
-        url_list.append(url)
-        #print(url)
-    url_list = set(url_list)
-    return title,url_list
+    async def connect_url(self,url):
+        if url is None:
+            return None
+        self.count += 1
+        print("\n",self.count,".\tConnect url :",url,"\tuse time:",time.time()-self.t0)
+        i = 5
+        for _ in range(6):
+            headers = random.sample(self.headers_pool,1)[0]
+            cookies = random.sample(self.cookies_pool,1)[0]
+            try:
+                async with aiohttp.ClientSession(cookies = cookies) as session:
+                    async with session.get(url,headers = headers) as r:
+                        html =await r.text()
+                        await asyncio.sleep(self.time)
+                        return html
+            except:
+                print("\nConnect url:",url,"sleep 5s then retry 剩余次数：",i)
+                i -= 1
+                await asyncio.sleep(10)
+        print("Wait for 30s then get cookies")
+        await asyncio.sleep(30)
+        t1 = time.time() - t0
+        self.cookies_pool = []
+        self.cookies_pool = self.init_cookies()
+        self.t0 = t1
+        self.connect_url(url)
+
 
 def analyse_att_url(html):
-        p = re.compile(r'href=\\"(\\/\\/weibo.com(\\/p)?\\/\d+\\/follow\?from=page_\d+&wvr=6&mod=headfollow#place)\\"')
-        try :
-            url1 = p.search(html)
-            url1 = url1.group(1)
-            url2 = re.sub('/','',url1)
-            soup = BeautifulSoup(html,'lxml')
-            url = 'https:' + re.sub(re.compile(r'\\'),'/',url2)
-            #print(url)
-        except:
-            print('search attaiton url error')
-            return 'https://weibo.com/6562848155/follow?rightmod=1&wvr=6'
-        return url
+    if html is None:
+        return None
+    html1 = re.sub(re.compile(r'\\/'),"/",html)
+    html2 = re.sub(re.compile(r'\\"'),"\"",html1)
+    html3 = re.sub(re.compile(r'\\t'),"\t",html2)
+    html4 = re.sub(re.compile(r'\\n'),"\n",html3)
+    html5 = re.sub(re.compile(r'\\r'),"\r",html4)
+    p = re.compile('href="(/\d+/follow\?rightmod=\d&wvr=\d)"')
+    p1 = re.compile(r'<span\s+class="item_text\s+W_fl">\s*([^<>\s]+)\s*[^<>\s]*</span>') 
+    p2 = re.compile(r'<title>([^<>]+)</title>')
+    p3 = re.compile(r'href="//weibo.com(/p/\d+/follow\?from=page_\d+&wvr=6&mod=headfollow#place)"')
+    try:
+        title = p2.search(html5).group(1).split("的微博")[0]
+    except:
+        print("\nSearch title error ...")
+        return None,None
+    try:
+        match = p3.search(html5)
+        if match is None:
+            match = p.search(html5)
+        url = "https://weibo.com" + match.group(1)
+    except:
+        print("\nSearch url error ...")
+        print("\n...................Error Title:",title)
+        return None,None
+    try:
+        location = p1.search(html5).group(1)
+    except:
+        print("\nSearch location error ...")
+        print("\n...................Error Title:",title)
+        return url,None
+    print("\nGet the url:",url,"from ",title,"\t位置：",location)
+    return url,location
 
-def analyse_some_msg(url,driver,cookies):
-    driver.add_cookie(cookies)
-    driver.get(url)
-    time.sleep(1)
-    html = driver.page_source
-    print("\n\n\n\n\nddddddddddddddddddddddd\n\n\n\n\n")
-    print(html)
-    driver.delete_all_cookies()
-    driver.close()
-
-def init_browser():
-    options = Options()
-    options.add_argument("--headless")
-    options.add_argument('--no-sandbox')
-    options.add_argument('start-maximized')
-    options.add_argument('window-size=1920x1080')
-    options.add_argument("--mute-audio")
-    options.add_argument('disable-infobars')
-    options.add_argument("--disable-extensions")
-    browser = webdriver.Chrome(executable_path = r'/usr/local/bin/chromedriver',options=options)
-    return browser
+def analyse_some_att(html):
+    if html is None:
+        return None
+    html1 = re.sub(re.compile(r'\\/'),"/",html)
+    html2 = re.sub(re.compile(r'\\"'),"\"",html1)
+    html3 = re.sub(re.compile(r'\\t'),"\t",html2)
+    html4 = re.sub(re.compile(r'\\n'),"\n",html3)
+    html5 = re.sub(re.compile(r'\\r'),"\r",html4)
+    p = re.compile(r'action-data=\\"uid=(\d{1,15})&nick=(.+?)\\">')
+    p2 = re.compile(r'<title>([^<>]+)</title>')
+    title = p2.search(html5).group(1).split("的微博")[0]
+    try:
+        att = p.findall(html)
+        url_list = []
+        for _id in att:
+            url = 'https://weibo.com/u/' + _id[0]# + '?from=myfollow_all'
+            url_list.append(url)
+            print("\nGet the user url:",url,"from ",title)
+    except:
+        return None
+    return set(url_list)
 
 
 async def main(loop):
     pool = mp.Pool(4)
-    cookies_pool = []
     url = "https://weibo.com/u/3195299207?from=myfollow_all"
-    count = 1
     base_url = 'https://weibo.com/'
-    i = 1
     seen = set()
     unseen = set([base_url])
-    browser = init_browser()
-    while (len(cookies_pool) < 20) and (base_url is not None):
-        user = Launcher('18545588767','19960419jh')
-        session = requests.session()
-        user.SetSession(session)
-        base_url,cookies = user.login()
-        print('get cookies No.',i)
-        i += 1
-        cookies_pool.append(cookies)
-        time.sleep(1)
-    print('cookies_pool len:',len(cookies_pool))
-    analyse_some_msg(url,browser,random.sample(cookies_pool,1)[0])
-    while True:  
-        #async with aiohttp.ClientSession(cookies=random.sample(cookies_pool,1)[0] ) as session:
-            #user.SetSession(session)
-        while len(unseen) != 0:
-            print('\nget  attation url ing ...')
-            tasks = [loop.create_task(user.get_att_url(url,random.sample(headers_group,1)[0],random.sample(cookies_pool,1)[0])) for url in unseen]
-            finished,unfinished = await asyncio.wait(tasks)
-            htmls = [f.result() for f in finished]
-            time.sleep(1)
-            print('\nanalyse attation html ing ...')
-            parse_jobs = [pool.apply_async(analyse_att_url,args=(html,)) for html in htmls]
-            att_urls = [j.get() for j in parse_jobs]
-            time.sleep(1)
-            print('\nget some attation ing ...')
-            tasks = [loop.create_task(user.get_some_att(url,random.sample(headers_group,1)[0],random.sample(cookies_pool,1)[0])) for url in att_urls]
-            finished,unfinished = await asyncio.wait(tasks)
-            htmls = [f.result() for f in finished]
-            time.sleep(1)
-            print('\nanalyse some attation ing ...')
-            parse_jobss = [pool.apply_async(analyse_some_att,args=(html,)) for html in htmls]
-            url_lists = [j.get() for j in parse_jobss]
-            print('\nupdata seen ing ...')
-            seen.update(unseen)
-            unseen.clear()
-            for title,url_list in url_lists:
-                print(count,' ',title)
-                unseen.update(url_list - seen)
-                count += 1
-            time.sleep(3)
+    user = Scrapy()
+    while len(unseen) != 0:
+        print('\nGet  attation url ing ...')
+        tasks = [loop.create_task(user.connect_url(url)) for url in unseen]
+        finished,unfinished = await asyncio.wait(tasks)
+        htmls = [f.result() for f in finished]
 
-            print('\nanalyse some message...')
-            parse_jobsss = [pool.apply_async(analyse_some_msg,args=(url,browser,random.sample(cookies_pool,1)[0])) for url in unseen]
-            print('use time:',time.time()-t1,'get user number:',count)
+        print('\nAnalyse attation html ing ...')
+        parse_jobs = [pool.apply_async(analyse_att_url,args=(html,)) for html in htmls]
+        user_datas = [j.get() for j in parse_jobs]
+        att_urls = []
+        location_list = set()
+        for url,location in user_datas:
+            if url is None:
+                continue
+            att_urls.append(url)
+            if location is None:
+                continue
+            if location in location_list:
+                locations[location] += 1
+            else:
+                locations = pd.Series([1],index = [location])
+        if len(location_list) > 0:    
+            print("\nLocations:\n",locations)
+
+        print('\nGet some attation ing ...')
+        tasks = [loop.create_task(user.connect_url(url)) for url in att_urls]
+        finished,unfinished = await asyncio.wait(tasks)
+        htmls = [f.result() for f in finished]
+
+        print('\nAnalyse some attation ing ...')
+        parse_jobss = [pool.apply_async(analyse_some_att,args=(html,)) for html in htmls]
+        url_lists = [j.get() for j in parse_jobss]
+
+        print('\nUpdata seen ing ...')
+        seen.update(unseen)
+        unseen.clear()
+
+        for url_list in url_lists:
+            unseen.update(url_list - seen)
+
+        print('Use time:',time.time()-user.t0,'get user number:',len(seen)+len(unseen))
 
 
 if __name__ == "__main__":
-    file= open('headers','r')
-    headers_group = []
-    for headers in file.readlines():
-        headers = {"User-Agent":headers.strip('\n')}
-        headers_group.append(headers)
-    t1 = time.time()
     loop = asyncio.get_event_loop()
     loop.run_until_complete(main(loop))
     loop.close()
